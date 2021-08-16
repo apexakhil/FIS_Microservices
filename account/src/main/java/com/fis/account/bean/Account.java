9a4f22695210d56ef7f36f2ed26edc4b71a55914
apexakhil007@gmail.com <apexakhil007@gmail.com>
2021-08-16 13:03:46 +0530

    added loan microservice

---
+++ account/src/main/java/com/fis/account/bean/Account.java
@@ -0,0 +1,45 @@
+package com.fis.account.bean;
+
+public class Account {
+	private long number;
+	private String type;
+	private int balance;
+	
+	public Account(long number, String type, int balance) {
+		super();
+		this.number = number;
+		this.type = type;
+		this.balance = balance;
+	}
+
+	public long getNumber() {
+		return number;
+	}
+
+	public void setNumber(long number) {
+		this.number = number;
+	}
+
+	public String getType() {
+		return type;
+	}
+
+	public void setType(String type) {
+		this.type = type;
+	}
+
+	public int getBalance() {
+		return balance;
+	}
+
+	public void setBalance(int balance) {
+		this.balance = balance;
+	}
+
+	@Override
+	public String toString() {
+		return "Account [number=" + number + ", type=" + type + ", balance=" + balance + "]";
+	}
+	
+	
+}
